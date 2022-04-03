731a2342faf03cb90c53ce6c7066e872a7ff1372
Userluckytian <tianlukang123@163.com>
2022-04-03 23:49:30 +0800

    🚧 fix：实现一个简单的样式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dark-mode';
+  themeNameBtn: string | null = 'light';
+
+  changeTheme() {
+    const body = document.getElementsByTagName('body')[0]; // 获取dom
+    const theme = body.getAttribute('app-theme-style'); // 获取属性名
+    if (theme === 'light') {
+      this.themeNameBtn = theme; // 这是themeNameBtn是为了在页面上展示
+      body.setAttribute('app-theme-style', 'dark'); // 设置新的属性名
+    } else {
+      this.themeNameBtn = theme;
+      body.setAttribute('app-theme-style', 'light');
+    }
+  }
+
 }
